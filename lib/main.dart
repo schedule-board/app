5de7cd2317ebbc54238ea4d404960be1c2f1f691
sami-bre <samuelbirhanu121@gmail.com>
2023-05-10 22:58:29 +0300

    Installed the go_router package and configured navigation 2.0 on the main page.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:schedule/blocs/auth_bloc.dart';
+import 'package:schedule/ui/landing_page.dart';
+import 'package:schedule/ui/login_view.dart';
 import 'package:schedule/ui/signup.dart';
 import 'package:schedule/ui/subject_detail.dart';
 import 'package:schedule/ui/announce.dart';
@@ -8,25 +11,58 @@ import 'package:schedule/ui/announce.dart';
 // import 'announcement.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final router = GoRouter(
+    initialLocation: '/login',
+    initialExtra: GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginView(),
+    ),
+    routes: [
+      // a route for the root of the app
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginView(),
+      ),
+      GoRoute(
+        path: '/landing',
+        builder: (context, state) => LandingPage(),
+      ),
+      GoRoute(
+        path: '/signUp',
+        builder: (context, state) => const SignUpView(),
+      ),
+      GoRoute(
+        path: '/subjectDetail',
+        builder: (context, state) => const SubjectDetailPage(),
+      ),
+    ],
+  );
 
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-        BlocProvider<AuthBloc>(create: (context) => AuthBloc(),)
+        BlocProvider<AuthBloc>(
+          create: (context) => AuthBloc(),
+        )
       ],
-      child: MaterialApp(
+      child: MaterialApp.router(
+        routerDelegate: router.routerDelegate,
+        routeInformationParser: router.routeInformationParser,
         title: 'Schedule Board',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home:
-            const SignUpView(), // this will be changed. we're doing this temporarily tot test the SubjectDetailPage
       ),
     );
   }
